a61a0b90cb65e93c18207f7615988049681db129
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-20 16:36:27 -0300

    Update StackTest.cpp

--- Algoritmos/Stack/Enlazada/StackTest.cpp
+++ Algoritmos/Stack/Enlazada/StackTest.cpp
@@ -4,16 +4,15 @@
 
 static Stack Tstack;
 
-/*int main(){		
-			push(Tstack, 1);
-			push(Tstack, 2);
-			vaciarStack(Tstack);
-			
-			pop(Tstack);
-			assert(Tstack.nivel != 0);
-			vaciarStack(Tstack);
+	int main(){		
+		push(Tstack, 1);
+		push(Tstack, 2);
+		vaciarStack(Tstack);
 		
-			top(Tstack);
-			assert(Tstack.nivel != 0);
-}*/
-
+		pop(Tstack);
+		assert(Tstack.nivel != 0);
+		vaciarStack(Tstack);
+		
+		top(Tstack);
+		assert(Tstack.nivel != 0);
+		}
