1cd2c191fc8fb48a5457cbd1d205ad1498a70bad
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-22 18:53:51 -0300

    Update QueueEnlazadoTest.cpp

--- Algoritmos/Queue/Enlazada/QueueEnlazadoTest.cpp
+++ Algoritmos/Queue/Enlazada/QueueEnlazadoTest.cpp
@@ -1,45 +1,22 @@
 #include "stdafx.h"
-#include <iostream>
-#include <assert.h>
-#include <array>
 #include "QueueEnlazado.h"
-#include <Windows.h>
-using namespace std;
+#include <assert.h>
 
 static Queue TQueue;
-static int a = 0, c;
 
 int main() {
 
-	cout << "Seleccione la funcion a testear:" << endl << "1.enQueue" << endl << "2.eQueue" << endl << "3.First" << endl;
-	cin >> c;
-	assert(c > 0 && c < 4);
-	switch (c)
-	{
-	case 1:
-		cout << "Ingrese la cantidad de elementos genericos a ingresar a la Queue: ";
-		unsigned N;
-		cin >> N;
-		assert(N > 0);
-		cout << "El Queue es: " << endl;
-		while (a<N)
-		{
-			enQueue(TQueue, rand() % 100);
-			cout << "[" << TQueue.rear->value << "]" << endl;
-			++a;
-		}
-		cin >> a;
-		break;
-	case 2:
-		vaciarQueue(TQueue);
-		cout << "Intentando hacer deQueue a una Queue vacia...";
-		Sleep(3000);
-		deQueue(TQueue);
-	case 3:
-		vaciarQueue(TQueue);
-		cout << "Intentando obtener el primer elemento de una Queue vacia...";
-		Sleep(3000);
-		first(TQueue);
-	}
+	enQueue(TQueue, 1);
+	enQueue(TQueue, 1);
+	deQueue(TQueue);
+	deQueue(TQueue);
 
+	vaciarQueue(TQueue);
+
+	deQueue(TQueue);
+	assert(TQueue.nivel != 0);
+	vaciarQueue(TQueue);
+
+	first(TQueue);
+	assert(TQueue.nivel != 0);
 }
