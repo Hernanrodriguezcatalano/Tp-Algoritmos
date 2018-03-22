8bad0b8d7508926cfd1fb6b8a73b55a7d6e02eb4
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-22 18:52:46 -0300

    Update QueueEnlazado.cpp

--- Algoritmos/Queue/Enlazada/QueueEnlazado.cpp
+++ Algoritmos/Queue/Enlazada/QueueEnlazado.cpp
@@ -1,10 +1,9 @@
 #include "stdafx.h"
 #include<iostream>
 #include "QueueEnlazado.h"
-#include <cassert>
-#include <array>
 
 using namespace std;
+
 void enQueue(Queue& t, int y) {
 	if (t.nivel == 0) {
 		t.rear->value = y;
@@ -19,17 +18,27 @@ void enQueue(Queue& t, int y) {
 	}
 }
 int deQueue(Queue& t) {
-	assert(t.nivel != 0);
+
+		if (t.nivel != 0) {
 		int valor = t.front->value;
 		t.front = t.front->next;
 		--t.nivel;
 		return valor;
-	
+	}
+	else
+	{
+		cout << "Queue vacia.";
+	}
 }
+
 int first(const Queue& t) {
-	assert(t.nivel != 0);
+	if (t.nivel != 0) {
 		return t.front->value;
-	
+	}
+	else
+	{
+		cout << "Queue Vacia.";
+	}
 }
 unsigned length(const Queue& t) {
 	return t.nivel;
@@ -38,48 +47,4 @@ void vaciarQueue(Queue& t) {
 	t.front = new Node;
 	t.rear = t.front;
 	t.nivel = 0;
-}
-
-bool EsPalindromo(Queue t) {
-	assert(t.nivel != 0);
-
-	unsigned Long = t.nivel;
-	Queue Qaux;
-	Queue Qaux2;
-	Qaux = t;
-
-	for (size_t i = 0; i < Long; i++)
-	{
-		enQueue(Qaux2, deQueue(t));
-	}
-
-	for (size_t i = 1; i <= Long; i++)
-	{
-		if (i>Long)
-		{
-			return true;
-		}
-		cout << "Q.rear->value:" << Qaux.rear->value <<" posicion :"<<i << endl;
-		cout << "Q.rear->front:" << Qaux.front->value <<" posicion :"<<i << endl;
-		cout << "Q2.rear->value:" << Qaux2.rear->value <<" posicion :"<<i << endl;
-		cout << "Q2.front->value:" << Qaux2.front->value <<" posicion :"<<i << endl;
-		Qaux.front->next;
-		Qaux2.front->next;
-	}
-
-/*	for (size_t i = 0; i < Long; i++)
-	{
-		if (Qaux.rear->value != Qaux2.front->value)
-		{
-			cout << "error en: " << i<<endl;
-			return false;
-		}
-		cout << "Qaux.rear Antes del next: "<<Qaux2.rear->value<<endl <<"vuelta: "<<i <<endl;
-		Qaux.rear->next;
-		--Qaux2.nivel;
-		cout << "Qaux.rear Despues del next: " << Qaux2.rear->value << endl << "vuelta: " << i << endl; 
-	}
-	return true;
-	*/
-
 }
