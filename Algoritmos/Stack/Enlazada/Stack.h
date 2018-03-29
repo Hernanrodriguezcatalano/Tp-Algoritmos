10a0dfd78524266fbaee2fb4784fb0a746b3c585
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 16:17:42 -0300

    Update Stack.h

--- Algoritmos/Stack/Enlazada/Stack.h
+++ Algoritmos/Stack/Enlazada/Stack.h
@@ -1,18 +1,18 @@
 #include<iostream>
 
-
+const int N = 32767;
 struct Nodo {
 	int value;
 	Nodo *next = nullptr;
 };
 
-struct Stack {
+struct stack {
 	Nodo *first = nullptr;
 	unsigned nivel = 0;
 };
 
-void push(Stack&, int);
-int pop(Stack&);
-int top(const Stack&);
-unsigned length(const Stack&);
-void vaciarStack(Stack&); 
+void push(stack&, int);
+int pop(stack&);
+int top(const stack&);
+unsigned length(const stack&);
+void vaciarStack(stack&);
