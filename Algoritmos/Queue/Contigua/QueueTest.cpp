7a4f97a7b9d4e2d3b7854214afb93e4f1e2d9be2
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 16:50:56 -0300

    Update QueueTest.cpp

--- Algoritmos/Queue/Contigua/QueueTest.cpp
+++ Algoritmos/Queue/Contigua/QueueTest.cpp
@@ -2,23 +2,39 @@
 #include <cassert>
 #include "Queue.h"
 
-static queue Tqueue;
+queue TQueue;
 
 int main() {
 
-	for (size_t i = 0; i <= M; i++)
-	{
-		enQueue(Tqueue, 1);
-	}
-	assert(Tqueue.size_t < M);
+	enQueue(TQueue, 1);
+	enQueue(TQueue, 2);
+	enQueue(TQueue, 3);
+	enQueue(TQueue, 4); 
+	assert(TQueue.size_t < M);
+	assert(TQueue.size_t > 0);
 
-	vaciarQueue(Tqueue);
+	first(TQueue);
+
+	vaciarQueue(TQueue);
+
+	enQueue(TQueue, 5);
+	enQueue(TQueue, 6);
+	enQueue(TQueue, 7);
+	enQueue(TQueue, 8); 
+	assert(TQueue.size_t < M);
+	assert(TQueue.size_t > 0);
+
+	first(TQueue);
+
+	deQueue(TQueue);
+	deQueue(TQueue);
+	deQueue(TQueue);
+	assert(TQueue.size_t < M);
+	assert(TQueue.size_t > 0);
+
+	first(TQueue);
+
+	deQueue(TQueue);
+	assert(TQueue.size_t < M);
 	
-	deQueue(Tqueue);
-	assert(Tqueue.size_t > 0);
-	
-	vaciarQueue(Tqueue);
-	
-	first(Tqueue);
-	assert(Tqueue.size_t > 0);
 }
