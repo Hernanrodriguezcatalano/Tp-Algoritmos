e187edd31e46640eb6fcbe5b277569800407e67f
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 16:49:48 -0300

    Update QueueApp.cpp

--- Algoritmos/Queue/Contigua/QueueApp.cpp
+++ Algoritmos/Queue/Contigua/QueueApp.cpp
@@ -1,65 +1,73 @@
 #include "stdafx.h"
 #include <iostream>
 #include "Queue.h"
+#include <array>
 
 using namespace std;
-static unsigned a;
-static int c;
-static queue Tqueue;
 
-bool EsPalindromo(queue);
+unsigned a = 1;
+
+int c;
+queue TQueue;
+
+bool EsPalindromo(queue q);
+
 int main() {
-
-	while (a=1) {
-		cout << "Ingresa un para para introducir a la Queue: ";
+	while (a<2) {
+		cout << "Ingresa un para para introducir la Queue: ";
 		cin >> c;
-		enQueue(Tqueue, c);
-		cout << "1.Para introducir otro valor.[MAX 800]" << endl << "2.¿Es Palindromo?." << endl;
+		enQueue(TQueue, c);
+		cout << "1.Para introducir otro valor.[Max 800]" << endl << "2.Para continuar." << endl;
 		cin >> a;
-		while (a<1 && a>2){
-			cout << "Ingrese un valor valido";
-			cout << "1.Para introducir otro valor.[MAX 800]" << endl << "2.¿Es Palindromo?." << endl;
-			cin >> a;
+		if (a>0 && a<3) {}
+		else
+		{
+			while (!(a > 0 && a < 3))
+			{
+				cout << "Ingrese un valor valido." << endl << endl;
+				cout << "1.Para introducir otro valor.[Max 800]" << endl << "2.Para continuar." << endl;
+				cin >> a;
+			}
 		}
+
 	}
 
-	cout << "La Queue: " << endl;
-	for (size_t i = 0; i < Tqueue.size_t; i++)
+	if (EsPalindromo(TQueue))
 	{
-		cout << "[" << Tqueue.b[i] << "]"<<endl;
-	}
 
-	if (EsPalindromo(Tqueue))
-	{
 		cout << "Es Palindromo";
 		cin >> a;
 	}
 	else
 	{
+
 		cout << "No es Palindromo";
 		cin >> a;
 	}
 }
 
-bool EsPalindromo(queue t) {
-	queue qaux;
-	int Long = t.size_t;
-	
+bool EsPalindromo(queue s) {
+
+	array <int, M> arraux;
+	queue TQueue;
+	unsigned Long = s.size_t;
+
 	for (size_t i = 0; i < Long; i++)
 	{
-		enQueue(qaux, deQueue(t));
-
+		arraux.at(i) = deQueue(s);
+		enQueue(TQueue, arraux.at(i));
 	}
-
+	cout << "La Queue: " << endl;
 	for (size_t i = 0; i < Long; i++)
 	{
-
-		if (t.b[i] != qaux.b[Long - 1])
+		cout << "[" << arraux.at(i) << "]" << endl;
+	}
+	for (size_t i = 0; i < Long; i++)
+	{
+		if (deQueue(TQueue) != arraux.at(Long-1))
 		{
-			cout << "error en: " << i<<endl;
 			return false;
 		}
-		--Long;
-	}
-	return true;
+		Long--;
+	}return true;
 }
