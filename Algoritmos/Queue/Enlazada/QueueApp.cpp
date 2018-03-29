6da88711bb5af687fa463f22ae71274924e58cc2
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 18:05:17 -0300

    Update QueueApp.cpp

--- Algoritmos/Queue/Enlazada/QueueApp.cpp
+++ Algoritmos/Queue/Enlazada/QueueApp.cpp
@@ -1,82 +1,73 @@
 #include "stdafx.h"
 #include <iostream>
 #include "Queue.h"
+#include <array>
 
 using namespace std;
 
-bool EsPalindromo(Queue);
+unsigned a = 1;
 
-static Queue TQueue;
-static int a = 0, c;
-using namespace std;
+int c;
+queue Tqueue;
+
+bool EsPalindromo(queue q);
 
 int main() {
-
 	while (a<2) {
-		cout << "Ingresa un para para introducir al Stack: ";
+		cout << "Ingresa un para para introducir la Queue: ";
 		cin >> c;
-		enQueue(TQueue, c);
-		cout << "1.Para introducir otro valor" << endl << "2.¿Es Palindromo?" << endl;
+		enQueue(Tqueue, c);
+		cout << "1.Para introducir otro valor.[Max 800]" << endl << "2.Para continuar." << endl;
 		cin >> a;
+		if (a>0 && a<3) {}
+		else
+		{
+			while (!(a > 0 && a < 3))
+			{
+				cout << "Ingrese un valor valido." << endl << endl;
+				cout << "1.Para introducir otro valor.[Max 800]" << endl << "2.Para continuar." << endl;
+				cin >> a;
+			}
+		}
+
 	}
 
-	if (EsPalindromo(TQueue))
+	if (EsPalindromo(Tqueue))
 	{
+
 		cout << "Es Palindromo";
 		cin >> a;
 	}
 	else
 	{
+
 		cout << "No es Palindromo";
 		cin >> a;
 	}
 }
 
-bool EsPalindromo(Queue q) {
+bool EsPalindromo(queue s) {
 
-	const int N = 800;
-	array <int, N> arraux;
-	Queue TQueue;
-
-	unsigned Long = q.nivel;
+	array <int, M> arraux;
+	queue TQueue;
+	unsigned Long = s.size_t;
 
 	for (size_t i = 0; i < Long; i++)
 	{
-		arraux[i] = deQueue(q);
-		cout << "arraux: " << arraux[i] << endl;
+		arraux.at(i) = deQueue(s);
+		enQueue(TQueue, arraux.at(i));
 	}
-
+	cout << "La Queue: " << endl;
 	for (size_t i = 0; i < Long; i++)
 	{
-		enQueue(TQueue, arraux[i]);
+		cout << "[" << arraux.at(i) << "]" << endl;
 	}
-
 	for (size_t i = 0; i < Long; i++)
 	{
-		if (deQueue(TQueue) != arraux[Long-1])
+		if (deQueue(TQueue) != arraux.at(Long - 1))
 		{
-			cout << "Error en : "<< i<<endl;
 			return false;
 		}
 		Long--;
 	}return true;
-
-	//array <int, N> arraux2;  // version con 2 Arrays
-	/*for (size_t i = 0; i < Long; i++) 
-	{
-		arraux2[i] = arraux[i];
-		cout << "arraux2: " << arraux2[i] << endl;
-	}
-
-	for (size_t i = 0; i < Long ; i++)
-	{
-		if (arraux[i] != arraux2[Long - 1])
-		{
-			cout << "Error en :" << i << endl;
-			return false;
-		}
-		Long--;
-	}
-	return true;
-	*/
 }
