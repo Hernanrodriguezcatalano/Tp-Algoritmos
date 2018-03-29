7bc259348cb8e6878c6757b3927b048cbd1d6e9d
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 16:19:28 -0300

    Update StackTest.cpp

--- Algoritmos/Stack/Enlazada/StackTest.cpp
+++ Algoritmos/Stack/Enlazada/StackTest.cpp
@@ -1,21 +1,39 @@
 #include "stdafx.h"
-#include <cassert.h>
+#include <cassert>
 #include "Stack.h"
 
-static Stack Tstack;
+stack Tstack;
 
-	int main(){		
-			push(Tstack, 1);
-			push(Tstack, 2);
-			pop(Tstack);
-			pop(Tstack);
-			
-			vaciarStack(Tstack);
-			
-			pop(Tstack);
-			assert(Tstack.nivel != 0);
-			vaciarStack(Tstack);
-		
-			top(Tstack);
-			assert(Tstack.nivel != 0);
+int main() {
+
+	push(Tstack, 1);
+	push(Tstack, 2);
+	push(Tstack, 3);
+	push(Tstack, 4);
+	assert(Tstack.nivel < N);
+	assert(Tstack.nivel > 0);
+
+	top(Tstack);
+
+	vaciarStack(Tstack);
+
+	push(Tstack, 5);
+	push(Tstack, 6);
+	push(Tstack, 7);
+	push(Tstack, 8);
+	assert(Tstack.nivel < N);
+	assert(Tstack.nivel > 0);
+
+	top(Tstack);
+
+	pop(Tstack);
+	pop(Tstack);
+	pop(Tstack);
+	assert(Tstack.nivel > 0);
+	assert(Tstack.nivel < N);
+
+	top(Tstack);
+
+	pop(Tstack);
+	assert(Tstack.nivel < N);
 }
