aee5b26b439b79ab30641136d82ce1fe39bc0a3f
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 18:03:37 -0300

    Update Queue.cpp

--- Algoritmos/Queue/Enlazada/Queue.cpp
+++ Algoritmos/Queue/Enlazada/Queue.cpp
@@ -4,47 +4,54 @@
 
 using namespace std;
 
-void enQueue(Queue& t, int y) {
-	if (t.nivel == 0) {
+void enQueue(queue& t, int y) {
+	Node *q = new Node;
+	if (t.size_t == 0) {
 		t.rear->value = y;
-		++t.nivel;
+		++t.size_t;
 	}
 	else {
-		Node *q = new Node;
 		q->value = y;
 		t.rear->next = q;
 		t.rear = q;
-		++t.nivel;
+		++t.size_t;
 	}
 }
-int deQueue(Queue& t) {
 
-		if (t.nivel != 0) {
+
+int deQueue(queue& t) {
+	if (t.size_t != 0) {
 		int valor = t.front->value;
 		t.front = t.front->next;
-		--t.nivel;
+		--t.size_t;
 		return valor;
 	}
 	else
 	{
-		cout << "Queue vacia.";
+		std::cout << "Queue Vacio.";
+		return 0;
 	}
+	
 }
 
-int first(const Queue& t) {
-	if (t.nivel != 0) {
+int first(const queue& t) {
+	if (t.size_t != 0) {
 		return t.front->value;
 	}
 	else
 	{
-		cout << "Queue Vacia.";
+		std::cout << "Queue Vacio.";
+		return 0;
 	}
+	
 }
-unsigned length(const Queue& t) {
-	return t.nivel;
+
+unsigned length(const queue& t) {
+	return t.size_t;
 }
-void vaciarQueue(Queue& t) {
+
+void vaciarQueue(queue& t) {
 	t.front = new Node;
 	t.rear = t.front;
-	t.nivel = 0;
+	t.size_t = 0;
 }
