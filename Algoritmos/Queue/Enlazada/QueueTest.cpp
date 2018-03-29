17ea9bcabd7b3c10aec9768a34190baed60f32ac
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-29 18:04:41 -0300

    Update QueueTest.cpp

--- Algoritmos/Queue/Enlazada/QueueTest.cpp
+++ Algoritmos/Queue/Enlazada/QueueTest.cpp
@@ -1,22 +1,38 @@
 #include "stdafx.h"
+#include <cassert>
 #include "Queue.h"
-#include <cassert.h>
 
-static Queue TQueue;
+queue TQueue;
 
 int main() {
-
 	enQueue(TQueue, 1);
-	enQueue(TQueue, 1);
-	deQueue(TQueue);
-	deQueue(TQueue);
-
-	vaciarQueue(TQueue);
-
-	deQueue(TQueue);
-	assert(TQueue.nivel != 0);
-	vaciarQueue(TQueue);
+	enQueue(TQueue, 2);
+	enQueue(TQueue, 3);
+	enQueue(TQueue, 4);
+	assert(TQueue.size_t < M);
+	assert(TQueue.size_t > 0);
 
 	first(TQueue);
-	assert(TQueue.nivel != 0);
+
+	vaciarQueue(TQueue);
+
+	enQueue(TQueue, 5);
+	enQueue(TQueue, 6);
+	enQueue(TQueue, 7);
+	enQueue(TQueue, 8);
+	assert(TQueue.size_t < M);
+	assert(TQueue.size_t > 0);
+
+	first(TQueue);
+
+	deQueue(TQueue);
+	deQueue(TQueue);
+	deQueue(TQueue);
+	assert(TQueue.size_t < M);
+	assert(TQueue.size_t > 0);
+
+	first(TQueue);
+
+	deQueue(TQueue);
+	assert(TQueue.size_t < M);
 }
