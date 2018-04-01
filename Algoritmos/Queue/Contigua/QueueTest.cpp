a62fa8a9ac9ea86529d07ed122003b23fdff3251
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-04-01 18:08:12 -0300

    Update QueueTest.cpp

--- Algoritmos/Queue/Contigua/QueueTest.cpp
+++ Algoritmos/Queue/Contigua/QueueTest.cpp
@@ -2,39 +2,29 @@
 #include <cassert>
 #include "Queue.h"
 
-queue TQueue;
-
 int main() {
 
-	enQueue(TQueue, 1);
-	enQueue(TQueue, 2);
-	enQueue(TQueue, 3);
-	enQueue(TQueue, 4); 
-	assert(TQueue.size_t < M);
-	assert(TQueue.size_t > 0);
+	queue q;
 
-	first(TQueue);
-
-	vaciarQueue(TQueue);
-
-	enQueue(TQueue, 5);
-	enQueue(TQueue, 6);
-	enQueue(TQueue, 7);
-	enQueue(TQueue, 8); 
-	assert(TQueue.size_t < M);
-	assert(TQueue.size_t > 0);
-
-	first(TQueue);
-
-	deQueue(TQueue);
-	deQueue(TQueue);
-	deQueue(TQueue);
-	assert(TQueue.size_t < M);
-	assert(TQueue.size_t > 0);
-
-	first(TQueue);
-
-	deQueue(TQueue);
-	assert(TQueue.size_t < M);
+	assert(IsEmpty(q));
+	enQueue(q, 1);
+	assert(first(q) == 1);
+	enQueue(q, 2);
+	assert(first(q) == 1);
+	enQueue(q, 3);
+	assert(first(q) == 1);
+	enQueue(q, 4);
+	assert(first(q) == 1);
 	
+	deQueue(q);
+	assert(first(q) == 2);
+	deQueue(q);
+	assert(first(q) == 3);
+	deQueue(q);
+	assert(first(q) == 4);
+
+
+	assert(!(IsEmpty(q)));
+	deQueue(q);
+	assert(IsEmpty(q));
 }
